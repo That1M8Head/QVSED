8bb77d0a71b5f760afd4b3269ae5ea9bb98bb751
Arsalan Kazmi <sonicspeed848@gmail.com>
2023-07-06 18:42:43 +0100

    Config file is now auto-generated if missing
    Also removed unnecessary config options.

--- config.py
+++ config.py
@@ -2,10 +2,4 @@
 font_family = ["JetBrains Mono", "Cascadia Code", "Consolas", "Menlo", "monospace"]
 font_size = 11
 
-# Colours
-# Yes, you have to use the British spellings
-font_colour = "#FFFFFF"
-foreground_colour = "#393F4A"
-border_colour = "#31353F"
-background_colour = "#282C34"
-scrollbar_colour = "#393F4A"
+# That's it for now.
--- main.py
+++ main.py
@@ -3,31 +3,26 @@ from PyQt5.QtGui import QKeySequence, QFont
 from PyQt5.uic import loadUi
 import os
 
-config_file = "config.py"
-
-if not os.path.isfile(config_file):
-    # Create the config file with default options
-    with open(config_file, 'w') as f:
-        f.write('''# Font
-font_family = ["JetBrains Mono", "Cascadia Code", "Consolas", "Menlo", "monospace"]
-font_size = 11
-
-# Colours
-# Yes, you have to use the British spellings
-font_colour = "#FFFFFF"
-foreground_colour = "#393F4A"
-border_colour = "#31353F"
-background_colour = "#282C34"
-scrollbar_colour = "#393F4A"
-''')
-        
-import config as qvsed_config
-
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         loadUi("QVSED.ui", self)
+
+        config_file = "config.py"
+
+        if not os.path.isfile(config_file):
+            self.echoArea_Update("config.py not found - created default config.")
+            
+            with open(config_file, 'w') as f:
+                f.write('''# Font
+font_family = ["JetBrains Mono", "Cascadia Code", "Consolas", "Menlo", "monospace"]
+font_size = 11
+
+# That's it for now.
+''')
+
+        import config as qvsed_config
 
         font = QFont()
         font.setFamilies(qvsed_config.font_family)
