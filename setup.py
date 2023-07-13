7ec7fc38d76a8391c184f76dc82a1698fe3477d8
Arsalan Kazmi <sonicspeed848@gmail.com>
2023-07-13 14:11:31 +0100

    Bump version to 1.5.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qvsed',
-    version='1.5.3',
+    version='1.5.4',
     author='Arsalan Kazmi',
     description='Qt-Based Volatile Small Editor',
     long_description="""QVSED is a volatile and small text editor.
